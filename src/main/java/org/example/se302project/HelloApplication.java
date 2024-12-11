a0259702582160a10db8c69b17ff502ead6a26a4
Elif Sude Ozmen <elifsude035@gmail.com>
2024-12-11 19:23:16 +0300

    Main page interface

--- src/main/java/org/example/se302project/HelloApplication.java
+++ src/main/java/org/example/se302project/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("CLASSROOM ASSIGNMENT SOFTWARE");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
